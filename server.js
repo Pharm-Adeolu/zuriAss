f07b515347d9079471fd70942259ee09cd154056
Adeolu Niyifadeyi <adeoluayobami13@gmail.com>
2022-06-16 20:28:09 +0300

    My first Commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs')
+
+const server = http.createServer((req, res)=>{
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        case '/contact':
+            path += 'contact.html';
+            break;
+        case '/home':
+            res.setHeader('Location', '/');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            break;    
+
+    }
+    fs.readFile(path, (err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.end(data);
+        }
+    })
+})
+
+
+server.listen(3000, 'localhost', ()=>{
+console.log('listening on port 3000')
+})
+
+
